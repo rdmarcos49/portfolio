869741e0a10b0a12b419031cb85dc10700838219
Roberto Marcos <roberdm49@gmail.com>
2022-12-11 19:21:37 -0300

    seo config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
   integrations: [
     compress({ css: false }),
-    robotsTxt(),
   ]
 });
