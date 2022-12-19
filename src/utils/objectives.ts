95a99d42eb59f4f58a331baca0b67a31774db982
Roberto Marcos <roberdm49@gmail.com>
2022-12-19 15:02:33 -0300

    changes in objectives

--- src/utils/objectives.ts
+++ src/utils/objectives.ts
@@ -7,21 +7,21 @@ export const listOfObjectives: ObjectiveItem[][] = [
   [
     {
       thumbnail: '/icons/statistics.png',
-      description: 'I want to learn and master the CI/CD process, in order to improve the workflow of the team that I working.',
+      description: 'One of my objectives is find new challenges; work with various technologies, differents products, differents teams, for example.',
     },
     {
       thumbnail: '/icons/conversation.png',
-      description: 'I think that anyone can learn something by helping their own teams to improve their git flows, deployments, tests on local and cloud environments, among others.',
+      description: 'I have been worked with differents work teams and I colaborated in differents products. There were times when it was difficult for me to understand the needs of the client, or when I had to do a lot of research to understand the technologies that were being used. Every time I overcame an obstacle I realized that I grew a little more, as a person and as a developer. I want to continue meeting challenges that allow me to constantly grow.',
     },
   ],
   [
     {
       thumbnail: '/icons/checklist.png',
-      description: 'I also want to master other developing areas, and learn their patterns, data structures, architectures.',
+      description: 'Another of my goals is to continue learning about the frontend and also other areas, as backend, cloud, ui/ux, between others.',
     },
     {
       thumbnail: '/icons/vscode.png',
-      description: 'Right now I\'m working only in the frontend, but I would glad to collaborate in other areas, in professional environments.',
+      description: 'Knowing other areas, besides the frontend in particular, allows me to better understand the needs of the teams. In this way, having knowledge of other sectors will allow me to support my work teams with greater efficiency.',
     },
-  ]
+  ],
 ]
