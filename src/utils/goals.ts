d864536ce0da2de2bdb2babfbab8387f5e67d1c4
Roberto <roberdm49@gmail.com>
2022-12-26 13:40:47 -0300

    goals modified

--- src/utils/goals.ts
+++ src/utils/goals.ts
@@ -7,17 +7,17 @@ export const listOfGoals: GoalItem[][] = [
   [
     {
       thumbnail: '/icons/graphic.png',
-      description: 'One of my goals is to work on projects focused on different aspects; scalability, high performance, user experience, SEO.',
+      description: 'To work on projects focused on scalability, high performance, user experience, and SEO, among other aspects.',
     },
   ],
   [
     {
       thumbnail: '/icons/checklist.png',
-      description: 'I also want to learn a little bit about other areas, such as backend, cloud, UI/UX.',
+      description: 'I also want to learn about other areas, such as backend, cloud, UI/UX.',
     },
     {
       thumbnail: '/icons/vscode.png',
-      description: 'Knowing about other areas allows me to better understand the needs of the teams. In this way, I would support my work teams with greater efficiency.',
+      description: 'Knowing about other areas gives me the chance to understand the needs of the product, and lets me work with the rest of my team in a more cohesive and efficient way.',
     },
   ],
 ]
